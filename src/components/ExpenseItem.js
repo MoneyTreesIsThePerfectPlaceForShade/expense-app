01d8c1a68c173b776e27fcfc22726c298e47864c
Money Trees <matveyshakur@gmail.com>
2022-10-10 15:43:07 +0500

    feat: added css for expense item and did some notes in readme

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
-import React from "react"
+import React from "react";
+import "./ExpenseItem.css";
 
 function ExpenceItem() {
-    return <h2>U AINT GOT NO WORRIIEE AT ALL</h2>
+    return (
+        <div className="expense-item">
+            <div>October 1st 2022</div>
+            <div className="expense-item__description">
+                <h2>Groot Fig</h2>
+                <div className="expense-item__price">$10</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenceItem;
