124e6affe80ac96d8030a20be2d827f8c220defa
Money Trees <matveyshakur@gmail.com>
2022-10-12 13:08:20 +0500

    feat: добавил первый useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenceDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenceItem = (props) => {
+    // тут применяется деструктуризация массива
+    // в этом реакт хуке первый элем массива содержит переменную
+    // а второй элемент содержит функцию, которая изменяет эту переменную при вызове
+    const [title, setTitle] = useState(props.title); // это реакт хук
+
+    const clickHandler = () => {
+        setTitle("UPDATED");
+    };
+
     return (
         <Card className="expense-item">
             <ExpenceDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{"$" + props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 };
